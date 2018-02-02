ea004a464c33b171919667a6fb4fd4babc7868df
shenchangmin <503228482@qq.com>
2018-02-02 18:13:25 +0800

    第二节done

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const utility = require('utility');
 const app = express();
 app.get('/', (req, res) => {
-    res.send('hello world');
+    const q = req.query.q;
+    const mad5Valur = utility.md5(q);
+    res.send(mad5Valur);
 });
 app.listen(3000, () => {
     console.log('app is listening at port 3000');
