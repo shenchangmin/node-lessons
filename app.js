eaf4ac19a77aeb043911ae8430b41d1d5e32b57f
shenchangmin <503228482@qq.com>
2018-02-22 11:43:30 +0800

    爬虫done

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const utility = require('utility');
+const superagent = require('superagent');
+const cheerio = require('cheerio');
 const app = express();
-app.get('/', (req, res) => {
-    const q = req.query.q;
-    const mad5Valur = utility.md5(q);
-    res.send(mad5Valur);
+app.get('/', (req, res, next) => {
+    superagent.get('https://cnodejs.org/').then(sres => {
+        const $ = cheerio.load(sres.text);
+        let items = [];
+        $('#topic_list').find('.topic_title').each(function (idx, element) {
+            let $element = $(element);
+            items.push({
+                title: $element.attr('title'),
+                href: $element.attr('href')
+            });
+        });
+
+        res.send(items);
+    }).catch(err => {
+        next(err);
+    });
 });
 app.listen(3000, () => {
     console.log('app is listening at port 3000');
